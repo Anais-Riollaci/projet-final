d05d6d0e6efbba9e81d4f29db0f3031b9c0f5e5b
MarieFTBR <mariegoulouzelle@gmail.com>
2020-04-09 18:49:39 +0200

    Add image property on animals

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -48,11 +48,18 @@ class Animal
     private $category;
 
     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Category")
      * @ORM\JoinColumn(nullable=false)
      */
 
-
+    public function getId(): ?int {
+        return $this->id;
+    }
 
     public function getName(): ?string
     {
@@ -122,6 +129,18 @@ class Animal
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
 
         return $this;
     }
